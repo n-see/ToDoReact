9ab6a051c8da3880aca76320c3de1f82cecf1465
Neo See <neococo123@yahoo.com>
2024-05-21 20:26:39 -0700

    finished just need to style more

--- toDoReact/src/Components/ToDo/ToDo.tsx
+++ toDoReact/src/Components/ToDo/ToDo.tsx
@@ -15,28 +15,34 @@ const ToDo = () => {
     const [list, setList] = useState<ToDoItem[]>([]);
 
     const [editInput, setEditInput] = useState(' ')
+    const [editId, setEditId] = useState<number | null>(null);
+
 
     //Create a function to help us add, delete, update
 
 
     //create a function to help us add our todo to our list
     const addToDo = (newItem:string) => {
-        const newToDo:ToDoItem = {
-
-            id: Math.random(),
-            todo: newItem,
-            completed: false
-        }
-        setList([...list, newToDo]);
-        setInput("");
-        console.log(list)
+        if(newItem != ""){
+            const newToDo:ToDoItem | any = {
+    
+                id: Math.random(),
+                todo: newItem,
+                completed: false
+            }
+            setList([...list, newToDo]);
+            setInput("");
+            console.log(list)
+        }else{
+            alert('Can not have an empty input')
+        } 
     }
     const completeFunctionTrue =(item:number) => {
-        setList(list.map(lists => lists.id === item ? {...lists, completed:true} : lists))
+        setList(list.map(lists => lists.id === item ? {...lists, completed:!lists.completed} : lists))
     }
-    const completeFunctionFalse =(item:number) => {
-        setList(list.map(lists => lists.id === item ? {...lists, completed:false} : lists))
-    }
+    // const completeFunctionFalse =(item:number) => {
+    //     setList(list.map(lists => lists.id === item ? {...lists, completed:false} : lists))
+    // }
     const deleteItem = (item:number) => {   
         console.log(item)
         // for(let i = 0; i < list.length; i++){
@@ -51,9 +57,17 @@ const ToDo = () => {
         // console.log(list[0].id)
         setList(list.filter(newList => newList.id !== item))
     }
-    // const handleUpdate = (itemNumber: number) => {
-    //     setList(list.map(lists => lists.id == itemNumber ? [{...list, todo: editInput}] : lists))
-    // }
+
+    const startEdit = (idNum:number, task:string) => {
+        setEditId(idNum);
+        setEditInput(task);
+    }
+
+    const handleUpdate = (itemNumber: number) => {
+        const updatedItem = list.map(lists => lists.id === itemNumber ? {...lists, todo: editInput} : lists)
+        setList(updatedItem);
+        // setList(list.map(lists => lists.id === itemNumber ? {...lists, todo: editInput} : lists))
+    }
         
   return (
     <>
@@ -69,14 +83,20 @@ const ToDo = () => {
             <ul>
                 {list.map(item => (
                     <li key={item.id} className="listComponent">
-                        {item.completed ? <FaRegCheckSquare onClick={() => completeFunctionFalse(item.id)} /> : <FaRegSquare  onClick={() => completeFunctionTrue(item.id)}/>}
+                        {item.completed ? <FaRegCheckSquare size={25} onClick={() => completeFunctionTrue(item.id)} /> : <FaRegSquare size={25}  onClick={() => completeFunctionTrue(item.id)}/>}
                         {item.todo}
                         <div>
                         <button className="btn" onClick={() => {deleteItem(item.id)}}>X</button>
                         {/* <button className="btn" onClick={()=> completeFunction(item.id)}>Complete</button> */}
                         </div>
-                        <input type="text" value={editInput} onChange={(e) => setEditInput(e.target.value)} />
-                        {/* <button className="addButton" onClick={() => handleUpdate(item.id)}> Edit</button> */}
+                        <button className="addButton" onClick={() => startEdit(item.id, item.todo)}> Edit</button>
+                        {/* Ternary op to display edit input field. */}
+                        {editId == item.id ? (<>
+                            <input type="text" value={editInput} onChange={(e) => setEditInput(e.target.value)} /> 
+                            <button className="addButton" onClick={() => handleUpdate(item.id)}>Save</button>
+                            <button className="addButton" onClick={() => setEditId(null)}>Cancel Edit</button>
+                        </>) : null}
+                        
                         
                     </li>
                 ))}
